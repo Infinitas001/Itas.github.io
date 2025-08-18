7ff5a4e8b6010711d248daadf5783ebc700fd427
Infinitas001 <1043095863@qq.com>
2025-08-18 10:44:45 +0800

    Fix GitHub Actions deploy workflow and site configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,7 +25,7 @@ import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://infinitas001.github.io/Itas.github.io/",
+	site: "https://infinitas001.github.io/",
 
 	base: "/",
 	trailingSlash: "always",
